012741f9af7c03669969ddf8e6cceaf280803ef0
cv21rgt <tsikoruz@gmail.com>
2021-08-03 15:19:56 +0100

    Added positional arguments

--- create_text_files.py
+++ create_text_files.py
@@ -6,28 +6,39 @@
 import pandas as pd
 import argparse
 import os
+import sys
 import uuid
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-b", "--filepath", required=True, help="filepath to csv file")
-#ap.add_argument("-n", "--column_name", required=True, help="name of column with data to convert to text file")
-#ap.add_argument("-sr", "--start_row", required=True, help="first row to start converting column data to text file")
-#ap.add_argument("-er", "-- last_row", required=True, help="last row to convert column data to text file")
-#ap.add_argument("-d", "--directory", required=True, help="directory/folder to save the text files to")
+# Create an argument parser
+parser = argparse.ArgumentParser()
 
-#args = vars(ap.parse_args())
+# Add the positional arguments
+parser.add_argument("filepath", help="filepath to csv file")
+parser.add_argument("column_name", help="name of column with data to convert to text file")
+parser.add_argument("start_row", help="first row to start converting column data to text file")
+parser.add_argument("last_row", help="last row to convert column data to text file")
+parser.add_argument("directory", help="directory/folder to save the text files to")
 
-args = ap.parse_args()
+args = parser.parse_args()
 
-if args.filepath:
-    print("Filepath to csv file.")
+csv_filepath = args.filepath
+column_name = args.column_name
+start_row = int(args.start_row)
+last_row = int(args.last_row)
+directory = args.directory
 
+# Read CSV file into a Pandas dataframe
+df = pd.read_csv(csv_filepath)
 
-
-# Extract input arguments
-
-# csv filename
-# column name 
-# start row
-# end row
-# path to folder to save the txt files 
+# Loop through dataframe from starting row to end row
+for idx in range(start_row, last_row + 1):
+    # Get the data at the intersection of row_index == idx and column == column_name
+    text = df.loc[idx, column_name]
+    # Path to text file with unique ID
+    text_filepath = os.path.join(directory, f"{str(uuid.uuid1())}.txt")
+    # Open file object for "writing"
+    file = open(text_filepath, "w")
+    # Write to file
+    file.write(text)
+    # Close file object
+    file.close()    
